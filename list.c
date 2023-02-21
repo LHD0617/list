0751c54f8d0394d57a0e6d4bec845941cc748b03
LHD0617 <320388825@qq.com>
2023-02-21 16:45:55 +0800

    202302211645创建 创建链表、删除链表函数

--- list.c
+++ list.c
@@ -13,3 +13,41 @@
 #include <stdlib.h>
 #include "list.h"
 
+/* @struct */
+#pragma pack(1) // 单字节对齐
+
+typedef struct
+{
+    list_node_t* next;          // 指针域
+    list_uint8 data[];          // 数据域
+}list_node_t;
+
+#pragma pack() // 恢复默认字节对齐
+
+/**
+ * @brief 创建list
+ * 
+ * @param size 单位数据大小
+ * @return list_cb_t* 对象指针 若为FIFO_NULL 则创建失败
+ */
+list_cb_t* list_create(list_uint8 size)
+{
+    list_cb_t* cb = (list_cb_t*)LIST_MALLOC(sizeof(list_cb_t));
+    if(!cb) return LIST_NULL;
+    cb->size = size;
+    cb->len = 0;
+    cb->next = LIST_NULL;
+    return cb;
+}
+
+/**
+ * @brief 删除链表
+ * 
+ * @param cb 对象指针
+ */
+void list_delete(list_cb_t cb)
+{
+    
+}
+
+#pragma pack() // 恢复默认字节对齐
--- list.h
+++ list.h
@@ -12,4 +12,45 @@
 #ifndef __LIST_H_
 #ifndef __LIST_H_
 
+/* @typedef */
+typedef unsigned char       list_uint8;
+typedef unsigned short int  list_uint16;
+typedef unsigned long int   list_uint32;
+typedef unsigned long long  list_uint64;
+
+typedef char                list_int8;
+typedef short int           list_int16;
+typedef long int            list_int32;
+typedef long long           list_int64;
+
+typedef list_int8           list_err;
+
+/* @define */
+#define         LIST_MALLOC                 malloc      // 空间申请接口
+#define         LIST_FREE                   free        // 空间释放接口
+
+#define         LIST_NULL                   NULL        // 空类型
+#define         LIST_ERROR_SUCCESS          0x00        // 成功
+#define         LIST_ERROR_NOTSPACE         0x01        // 空间不足
+
+/* @struct */
+#pragma pack(1) // 单字节对齐
+
+/**
+ * @brief 链表控制块
+ * 
+ */
+typedef struct
+{
+    list_uint8 size;            // 单位节点数据大小
+    list_uint32 len;            // 链表长度
+    list_node_t* next;          // 头指针
+}list_cb_t;
+
+#pragma pack() // 恢复默认字节对齐
+
+/* @Function declaration */
+list_cb_t* list_create(list_uint8 size);
+void list_delete(list_cb_t cb);
+
 #endif
