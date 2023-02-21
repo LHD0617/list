f9b9df9cdde6ba9c71ef8cc2ef19968556ff0ae7
LHD0617 <320388825@qq.com>
2023-02-21 16:01:34 +0800

    202302211601创建文件

---
+++ list.c
@@ -0,0 +1,15 @@
+/**
+ * @file list.c
+ * @author 满心欢喜
+ * @brief 链表
+ * @version 0.1
+ * @date 2023-02-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/* @include */
+#include <stdlib.h>
+#include "list.h"
+
---
+++ list.h
@@ -0,0 +1,15 @@
+/**
+ * @file list.h
+ * @author 满心欢喜
+ * @brief 链表
+ * @version 0.1
+ * @date 2023-02-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef __LIST_H_
+#ifndef __LIST_H_
+
+#endif
